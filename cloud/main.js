d99e83df4277283b26040d4782420eab4e023c83
Drin <drinvojka@gmail.com>
2018-04-14 09:10:03 +0200

    Pushtest5

--- cloud/main.js
+++ cloud/main.js
@@ -17,7 +17,6 @@ Parse.Cloud.define('pingReply', function(request, response) {
 
   Parse.Push.send({
   where: query,
-  channels : ['abcd'],
   // Parse.Push requires a dictionary, not a string.
   data: {"alert": "The Giants scored!"},
   }, { success: function() {
