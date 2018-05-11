e2e0f567e8300a12787f76383d37708c101e9107
Drin <drinvojka@gmail.com>
2018-05-11 13:55:52 +0200

    aftersave

--- cloud/main.js
+++ cloud/main.js
@@ -1,8 +1,37 @@
+Parse.Cloud.beforeSave('Match', function(request, response) {
 
+    if(!request.object.isNew()) {
+        // Retrieve the relationship information in json string format. 
+        // RELATION_QUEUE here is simply a string "queue"
+        var relQueueJsonStr = JSON.stringify(request.object.op("queue"));
+
+        if( relQueue !== undefined ) {
+            var relQueue = JSON.parse(relQueueJsonStr);
+            // Retrieve the operation being performed to this existing object.
+            // The value will be "AddRelation" if a the relation "queue" is  
+            // being added to this object
+            var operation = relQueue.__op;
+            if (operation == "AddRelation"){
+                console.log("Relation queue is being added");
+               // Do what you like here. In your case increment something
+
+                response.success();
+            } else {
+            // Relation is being removed
+                console.log("Relation queue is being removed");
+                response.success();
+            }
+        } else {
+            console.log("No queue relation.");
+            response.success();
+        }
+    } else {
+        response.success();
+    }
+});
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
-
 Parse.Cloud.define('userJoinRequest', function(request, response) {
 	
   var params = request.params;
@@ -31,7 +60,6 @@ Parse.Cloud.define('userJoinRequest', function(request, response) {
 
   response.success('success');
 });
-
 Parse.Cloud.define("getServerTime", function(request, response) {
 	
     var dateToday =  new Date();
