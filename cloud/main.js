21f16fdfc6976fc1ff1fa13b0762b9ccbc1bcd00
Drin <drinvojka@gmail.com>
2018-05-11 14:59:34 +0200

    aftersave5233

--- cloud/main.js
+++ cloud/main.js
@@ -1,10 +1,10 @@
-Parse.Cloud.beforeSave('Match', function(request, response) {
+Parse.Cloud.beforeSave(ClassConstants.Item.TYPE, function(request, response) {
 
     if(!request.object.isNew()) {
         // Retrieve the relationship information in json string format. 
         // RELATION_QUEUE here is simply a string "queue"
-		   console.log(" ******* OBJECT LOG ******** " + request.object.op);
-        var relQueueJsonStr = JSON.stringify(request.object.op("queue"));
+		   console.log(" ******* OBJECT LOG ******** " + request.object.op("AddRelation"));
+        var relQueueJsonStr = JSON.stringify(request.object.op(ClassConstants.Item.RELATION_QUEUE));
 		console.log("LOG " + relQueueJsonStr);
         if( relQueue !== undefined ) {
             var relQueue = JSON.parse(relQueueJsonStr);
