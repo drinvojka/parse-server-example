3d88c91dddf2e15829462aba8096711133cc4a9a
Drin <drin.vojka@albicommerce.com>
2018-12-01 12:06:57 +0100

    Update main.js
    Push

--- cloud/main.js
+++ cloud/main.js
@@ -106,15 +106,16 @@ Parse.Cloud.define('requestRejected', function(request, response) {
   var query = new Parse.Query(Parse.Installation);
   query.equalTo("installationId", sender);
 
-  Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"alert": hostUser + " rejected your request to join the match !" },
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
+ Parse.Push.send({
+	  channels: [ "All" ],
+	  data: {
+	    alert: "The Giants won against the Mets 2-3."
+	  }
+	}, {
+    useMasterKey: true
+  })
+  .then(function(red) {
+    response.success(red);
+  }, function(error) {
+    response.error("Error while trying to send push " + error.message);
+  });
