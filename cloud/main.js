e6f2108254da19c7cc795a72ca6abb217a8a3f65
Drin <drin.vojka@albicommerce.com>
2018-12-05 14:30:08 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -1,86 +1,39 @@
-Parse.Cloud.define('decrementPlayersLeft', function(request, response) {
-	
+// Android push test
+// To be used with:
+// https://github.com/codepath/ParsePushNotificationExample
+// See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
+
+Parse.Cloud.define('pushChannelTest', function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user
   var params = request.params;
-  var customData = params.matchId;
+  var user = request.user;
 
-
-
-  if (!customData) {
-    response.error("Missing customData!")
-  }
-  else
-  {
-	 const query = new Parse.Query("Match");
-     query.get(customData)
-    .then(function(match) {
-      match.increment("playersLeft",-1);
-      match.save();
-	  response.success("success")
-    })
-    .catch(function(error) {
-      console.error("Got an error " + error.code + " : " + error.message);
-    });
- 
-  }
-	   
-
-});
-Parse.Cloud.define('hello', function(req, res) {
-  res.success('Hi');
-});
-Parse.Cloud.define('userJoinRequest', function(request, response) {
-	
-  var params = request.params;
   var customData = params.customData;
-  var requestUser = params.username;
+  var launch = params.launch;
+  var broadcast = params.broadcast;
 
+  // use to custom tweak whatever payload you wish to send
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo("deviceType", "android");
 
-  if (!customData) {
-    response.error("Missing customData!")
+  var payload = {};
+
+  if (customData) {
+      payload.customdata = customData;
+  }
+  else if (launch) {
+      payload.launch = launch;
+  }
+  else if (broadcast) {
+      payload.broadcast = broadcast;
   }
 
-
-  var sender = JSON.parse(customData).sender;
-  var query = new Parse.Query(Parse.Installation);
-  query.equalTo("installationId", sender);
+  // Note that useMasterKey is necessary for Push notifications to succeed.
 
   Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"alert": requestUser + " requested to join your match !" },
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
-});
-Parse.Cloud.define("getServerTime", function(request, response) {
-	
-    var dateToday =  new Date();
-    response.success(dateToday);
-});
-Parse.Cloud.define('requestAccepted', function(request, response) {
-	
-  var params = request.params;
-  var customData = params.customData;
-  var hostUser = params.username;
-
-
-  if (!customData) {
-    response.error("Missing customData!")
-  }
-
-
-  var sender = JSON.parse(customData).sender;
-  var query = new Parse.Query(Parse.Installation);
-  query.equalTo("installationId", sender);
-
-  Parse.Push.send({
-  where: query,
-  // Parse.Push requires a dictionary, not a string.
-  data: {"alert": hostUser + " accepted your request to join the match !" },
+  where: pushQuery,      // for sending to a specific channel
+  data: payload,
   }, { success: function() {
      console.log("#### PUSH OK");
   }, error: function(error) {
@@ -90,16 +43,29 @@ Parse.Cloud.define('requestAccepted', function(request, response) {
   response.success('success');
 });
 
- Parse.Push.send({
-	  channels: [ "All" ],
-	  data: {
-	    alert: "The Giants won against the Mets 2-3."
-	  }
-	}, {
-    useMasterKey: true
-  })
-  .then(function(red) {
-    response.success(red);
-  }, function(error) {
-    response.error("Error while trying to send push " + error.message);
-  });
+// iOS push testing
+Parse.Cloud.define("iosPushTest", function(request, response) {
+
+  // request has 2 parameters: params passed by the client and the authorized user                                                                                                                               
+  var params = request.params;
+  var user = request.user;
+
+  // Our "Message" class has a "text" key with the body of the message itself                                                                                                                                    
+  var messageText = params.text;
+
+  var pushQuery = new Parse.Query(Parse.Installation);
+  pushQuery.equalTo('deviceType', 'ios'); // targeting iOS devices only                                                                                                                                          
+
+  Parse.Push.send({
+    where: pushQuery, // Set our Installation query                                                                                                                                                              
+    data: {
+      alert: "Message: " + messageText
+    }
+  }, { success: function() {
+      console.log("#### PUSH OK");
+  }, error: function(error) {
+      console.log("#### PUSH ERROR" + error.message);
+  }, useMasterKey: true});
+
+  response.success('success');
+});
