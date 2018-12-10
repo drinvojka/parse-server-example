cb908a38c36e85ed241f9cefe3d7b679a6fb6781
Drin <drin.vojka@albicommerce.com>
2018-12-10 20:56:47 +0100

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -76,7 +76,7 @@ Parse.Cloud.afterSave("JoinRequest", function(request) {
  var query = new Parse.Query("Match");
  var match = query.get(request.object.get("match").id);
  var hostUser = match.createdBy;
- var name = hostUser.getUsername();
+ var name = hostUser.get("Name");
   var pushQuery = new Parse.Query(Parse.Installation);
       pushQuery.equalTo('user', hostUser);
         Parse.Push.send({
